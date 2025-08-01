28cf7370eb484e09b2cdde0a86de2dec1b203c20
The Net Spy <thenetspy@RTGA.local>
2025-08-01 23:19:30 +0530

    Project Setup

---
+++ frontend-react/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>DMS RAG APP</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
